ff7877d72a079fdfde0b6bebd023ca7a5dd5b99a
Mille Loo <74830754+Pinkvinus@users.noreply.github.com>
2024-07-24 16:26:05 +0200

    Update Encoder.ino

--- arduino_teabagger/Encoder.ino
+++ arduino_teabagger/Encoder.ino
@@ -1,53 +1,82 @@
 // display setup 
 // https://docs.arduino.cc/learn/electronics/lcd-displays/
 
-#define pinA 11
-#define pinB 12
-#define encoderButtonpin 13
+#include <LiquidCrystal.h>
 
-const int maxMin = 99;
+#define rotaryPinA 11
+#define rotaryPinB 12
+#define rotaryButtonpin 13
+
+#define rs 7
+#define en 6
+#define d4 5
+#define d5 4
+#define d6 3
+#define d7 2
+
+const int maxSec = 5940;
 const int maxTemp = 100;
-float temp;
-float minutes;
+int temp;
+int seconds;
+unsigned long startTime;
+
 int aState;
 int aLastState;
 bool encoderButtonState;
 bool encoderButtonLastState;
 int globalState;
 
+char tempText[] = "Temp";
+char timeText[] = "Time left ";
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup() { 
-  pinMode(pinA, INPUT);
-  pinMode(pinB, INPUT);
-  pinMode(encoderButtonpin, INPUT);
+  // set rotary pins
+  pinMode(rotaryPinA, INPUT);
+  pinMode(rotaryPinB, INPUT);
+  pinMode(rotaryButtonpin, INPUT);
    
   Serial.begin(9600);
   // Reads the initial state of the outputA
-  aLastState = digitalRead(pinA);
-  encoderButtonLastState = digitalRead(encoderButtonpin);
+  aLastState = digitalRead(rotaryPinA);
+  encoderButtonLastState = digitalRead(rotaryButtonpin);
   globalState = 0;
   temp = 100;
-  minutes = 0;
+  seconds = 0;
+
+  lcd.begin(16, 2);
+  lcd.print(tempText);
+  lcd.setCursor(0, 1);
+  lcd.print(timeText);
 } 
 
 void loop() {
   if (globalState == 0){ // Select temp
     updateValEncoder(temp, maxTemp, 0, 5);
+    printTemp(int(temp));
     confirmSelection();
   }
   if (globalState == 1){ // Select time
-    updateValEncoder(minutes, maxMin, 0, 0.5);
+    updateValEncoder(seconds, maxSec, 0, 5);
+    printTime(seconds/60, seconds%60);
     confirmSelection();
+    startTime = millis();
   }
   if (globalState == 2){ // Timer countDown
+    countdown();
+    if(getSecLeft() <= 0){
+      globalState++;
+    }
+
   }
 }
 
-void updateValEncoder(float &val, int maxval, int minval, float interval){
-    aState = digitalRead(pinA); // Reads the "current" state of the outputA
+void updateValEncoder(int &val, int maxval, int minval, int interval){
+    aState = digitalRead(rotaryPinA); // Reads the "current" state of the outputA
     // If the previous and the current state of the outputA are different, that means a Pulse has occured
     if (aState != aLastState){     
       // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-      if (digitalRead(pinB) != aState) { 
+      if (digitalRead(rotaryPinB) != aState) { 
         if(val > minval) {val -=interval;} // 
       } else {
         if(val < maxval) {val +=interval;} // 
@@ -57,9 +86,8 @@ void updateValEncoder(float &val, int maxval, int minval, float interval){
     } 
     aLastState = aState; // Updates the previous state of the outputA with the current state
 }
-
 void confirmSelection(){
-  encoderButtonState = digitalRead(encoderButtonpin);
+  encoderButtonState = digitalRead(rotaryButtonpin);
   if (encoderButtonState !=encoderButtonLastState) {
     if (encoderButtonState == 0) {
       Serial.println("Button was pressed");
@@ -68,4 +96,52 @@ void confirmSelection(){
     }
     encoderButtonLastState = encoderButtonState;
   }
+}
+void temperature(){
+
+  lcd.setCursor(10, 0);
+
+  // Print the time left
+  lcd.print(22);
+
+  lcd.setCursor(12, 0);
+  lcd.print("C");
+
+}
+void countdown(){
+  unsigned long timeLeft = getSecLeft();
+  printTime(timeLeft/60, timeLeft%60);
+
+}
+long getSecLeft(){
+  //Returns the seconds remaining from the timer
+  unsigned long timeLeft = seconds > getElapsedSec(startTime) ? seconds - getElapsedSec(startTime) : 0;
+
+  return timeLeft;
+}
+long getElapsedSec(long time){
+  return (millis() - time) / 1000;
+}
+void printTime(long min, long sec){
+  lcd.setCursor(11, 1); 
+  if(min < 10){
+    lcd.print(0);
+  }
+  lcd.print(min);
+  lcd.print(":");
+  if (sec < 10) {
+    lcd.print(0);
+  }
+  lcd.print(sec);
+}
+void printTemp(int t){
+    lcd.setCursor(12, 0);
+    if (t < 100){ 
+      lcd.print(" ");
+    }
+    // Print the time left
+    lcd.print(t);
+
+    lcd.setCursor(15, 0);
+    lcd.print("C");
 }
